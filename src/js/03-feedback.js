f080d77cf63e060bbfa17b63d2f3bbd76bc59633
Andrii Bondar <77549085+BondarAV@users.noreply.github.com>
2023-05-28 17:15:08 +0200

    Adds files with all the solutions

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
+const form = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('input[name="email"]');
+const messageInput = document.querySelector('textarea[name="message"]');
 
+let storageObject = {};
+
+if (localStorage.getItem('feedback-form-state') === null) {
+  storageObject = {
+    email: '',
+    message: '',
+  };
+} else {
+  storageObject = JSON.parse(localStorage.getItem('feedback-form-state'));
+}
+
+function setDefaultInput(inputType, inputName) {
+  document.querySelector(`${inputType}[name="${inputName}"]`).defaultValue =
+    JSON.parse(localStorage.getItem('feedback-form-state'))[inputName];
+}
+
+setDefaultInput('input', 'email');
+setDefaultInput('textarea', 'message');
+
+form.addEventListener(
+  'input',
+  _.throttle(event => {
+    storageObject[event.target.name] = event.target.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(storageObject));
+  }, 500)
+);
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  console.log(formData);
+
+  localStorage.removeItem('feedback-form-state');
+
+  storageObject.email = '';
+  storageObject.message = '';
+
+  emailInput.value = '';
+  messageInput.value = '';
+});
